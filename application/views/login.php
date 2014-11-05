1a3f886899dbe8e801f20e2857b28b806f7c7d9d
raul <raul@zamacona.me>
2014-11-04 23:17:32 -0600

    rediseño, elementos definidos, imagenes, iconos, colores, botones, h1, h3, h4

--- application/views/login.php
+++ application/views/login.php
@@ -1,18 +1,18 @@
 <div class="[ contenido ] [ container ]">
-    <div class="[ col-xs-12 col-sm-8 col-md-6 ] [ center-block margin-bottom ]">
-        <div class="[ row ]">
-            <img class="col-xs-3 col-sm-2 center-block" src="<?php echo base_url().'assets/images/aeo-logo.png' ?>" alt="">
-        </div>
-        <div class="[ row ]">
-            <h3>Best Gift Ever</h3>
-        </div>
-        <div class="[ row ]">
-            <img class="col-xs-8 col-sm-6 center-block logo-main" src="<?php echo base_url().'assets/images/be.png' ?>" alt="">
-        </div>
-        <p class="text-center">El primer intercambio donde todos reciben lo que quieren</p>
-        <!-- <fb:login-button scope="public_profile,email" onlogin="checkLoginState();" class="btn btn-primary center-block"></fb:login-button> -->
-        <div class="text-center">
-            <a href="<?php echo $fb_login_url ?>" target="_top" class="btn btn-primary">Login</a>
+    <div class="[ row ] [ login ]">
+        <div class="[ col-xs-12 col-sm-8 col-md-6 ] [ center-block margin-bottom ]">
+            <div class="[ margin-bottom ]">
+                <h1>
+                    <a href="#">
+                        <img class="[ img-responsive ]" src="<?php echo base_url().'assets/images/logo-bge-white.png' ?>" alt="American Eagle Best Gift Ever" title="American Eagle Best Gift Ever" />
+                    </a>
+                </h1>
+            </div><!-- row -->
+            <h3 class="[ text-center ] [ margin-bottom-big ]">El primer intercambio donde todos reciben lo que quieren</h3>
+            <!-- <fb:login-button scope="public_profile,email" onlogin="checkLoginState();" class="btn btn-primary center-block"></fb:login-button> -->
+            <div class="[ text-center ]">
+                <a href="<?php echo $fb_login_url ?>" target="_top" class="btn btn-primary">Conecta con tu facebook <i></i> <img src="<?php echo base_url().'assets/images/icon-fb.png' ?>" alt="go"></a>
+            </div>
         </div>
     </div>
 </div><!-- login -->
