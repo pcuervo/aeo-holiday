1c168e2dfc4df07309d25ab0e37ae79ab8ccaf97
raul <raul@zamacona.me>
2014-11-05 10:50:23 -0600

    secret_friend

---
+++ application/views/secret_friend.php
@@ -0,0 +1,18 @@
+<div class="[ contenido ] [ container ]">
+    <div class="[ row ] [ login ]">
+        <div class="[ col-xs-12 col-sm-8 col-md-6 ] [ center-block margin-bottom ]">
+            <div class="[ margin-bottom ]">
+                <h1>
+                    <a href="#">
+                        <img class="[ img-responsive ]" src="<?php echo base_url().'assets/images/logo-bge-white.png' ?>" alt="American Eagle Best Gift Ever" title="American Eagle Best Gift Ever" />
+                    </a>
+                </h1>
+            </div><!-- row -->
+            <h3 class="[ text-center ] [ margin-bottom-big ]">El primer intercambio donde todos reciben lo que quieren</h3>
+            <!-- <fb:login-button scope="public_profile,email" onlogin="checkLoginState();" class="btn btn-primary center-block"></fb:login-button> -->
+            <div class="[ text-center ]">
+                <a href="<?php echo $fb_login_url ?>" target="_top" class="btn btn-primary">Conecta con tu facebook <i></i> <img src="<?php echo base_url().'assets/images/icon-fb.png' ?>" alt="go"></a>
+            </div>
+        </div>
+    </div>
+</div><!-- login -->
