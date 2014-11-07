b1d3d0b416df3224a21668f4e9c8cc4d6a57fb25
maichomper <maichomper@gmail.com>
2014-11-07 15:40:48 -0600

    video view

--- application/views/view_video.php
+++ application/views/view_video.php
@@ -3,6 +3,8 @@
         <div class="[ col-xs-12 col-sm-8 col-md-6 ] [ center-block margin-bottom ]">
             <video controls>
                 <source src="<?php echo $video_url; ?>" type="video/mp4">
+                <source src="<?php echo $video_url; ?>" type="video/mp4">
+                <source src="<?php echo $video_url; ?>" type="video/mp4">
             </video>    
         </div>
     </div>
