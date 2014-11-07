98ff650ae1966e9c88771bd68dc3dae74b4243e7
raul <raul@zamacona.me>
2014-11-06 23:45:13 -0600

    secret_friend view

--- application/views/secret_friend.php
+++ application/views/secret_friend.php
@@ -1,33 +1,31 @@
 <div class="[ contenido ] [ container ]">
     <h2 class="text-center">Mi amigo secreto</h2>
-    <div class="[ row ]">
-        <div class="[ col-xs-11] [ center-block ] [  ]">
-            <img class="[ img-circle ]" src="<?php echo $secret_friend['friend_picture'] ?>" alt="">
+    <div class="[ row ] [ margin-bottom ]">
+        <div class="[ col-xs-9 ] [ center-block ] [  ]">
+            <img class="[ img-responsive img-circle user-photo ]" src="<?php echo $secret_friend['friend_picture'] ?>" alt="">
         </div>
     </div><!-- row -->
-    <h3 class="[ text-center ]"><?php echo $secret_friend['name'] ?></h3>
-    <h3 class="[ text-center ]"><?php echo $secret_friend['group'] ?></h3>
+    <h4 class="[ text-center ]"><?php echo $secret_friend['name'] ?></h4>
+    <h4 class="[ text-center ] [ margin-bottom ]">(<?php echo $secret_friend['group'] ?>)</h4>
     <div class="[ row ]">
         <div class="[ col-xs-12 col-sm-8 col-md-6 ] [ center-block margin-bottom ]">
-
             <?php if($video == 0){ ?>
                 <div class="[ text-center ] [ margin-bottom ]">
-                    <a href="<?php echo base_url().'secret_friends/create_video/'.$secret_friend['group_friend_id'] ?>" target="_top" class="btn btn-primary">Grabar dedicatoria <i></i> <img src="<?php echo base_url().'assets/images/icon-go.png' ?>" alt="go"></a>
+                    <a href="<?php echo base_url().'secret_friends/create_video/'.$secret_friend['group_friend_id'] ?>" target="_top" class="btn btn-primary btn-go"><span>Grabar dedicatoria</span></a>
                 </div>
             <?php } else { ?>
                 <div class="[ text-center ] [ margin-bottom ]">
-                    <a href="<?php echo base_url().'secret_friends/view_video/'.$video['secret_friend_id'] ?>" target="_blank" class="btn btn-primary">Ver dedicatoria <i></i> <img src="<?php echo base_url().'assets/images/icon-go.png' ?>" alt="go"></a>
+                    <a href="<?php echo base_url().'secret_friends/view_video/'.$video['secret_friend_id'] ?>" target="_blank" class="btn btn-primary btn-go"><span>Ver dedicatoria</span></a>
                 </div>
             <?php } ?>
 
             <?php if($has_perfect_fit) { ?>
                 <div class="[ text-center ] [ margin-bottom ]">
-                    <a href="<?php echo base_url().'secret_friends/view_perfect_fit/'.$secret_friend['group_friend_id'] ?>" target="_top" class="btn btn-primary">Ver perfect fit <i></i> <img src="<?php echo base_url().'assets/images/icon-go.png' ?>" alt="go"></a>
-
+                    <a href="<?php echo base_url().'secret_friends/view_perfect_fit/'.$secret_friend['group_friend_id'] ?>" target="_top" class="btn btn-primary btn-go"><span>Ver perfect fit</span></a>
                 </div>
             <?php } ?>
              <div class="[ text-center ] [ margin-bottom ]">
-                <a href="<?php echo base_url().'secret_friends/create_message/'.$secret_friend['group_friend_id']  ?>" target="_top" class="btn btn-primary">Enviar mensaje <i></i> <img src="<?php echo base_url().'assets/images/icon-go.png' ?>" alt="go"></a>
+                <a href="<?php echo base_url().'secret_friends/create_message/'.$secret_friend['group_friend_id']  ?>" target="_top" class="btn btn-primary btn-go"><span>Enviar mensaje</span></a>
             </div>
         </div>
     </div>
