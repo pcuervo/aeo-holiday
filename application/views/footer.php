63dd75f09f45ef3202616fa6bf6f41daac3d540a
elaristi <alejandro@pcuervo.com>
2014-11-10 12:31:59 -0600

    something

--- application/views/footer.php
+++ application/views/footer.php
@@ -6,6 +6,7 @@
         <script src="<?php echo base_url().'assets/js/plugins.min.js' ?>"></script>
         <script src="<?php echo base_url().'assets/js/functions.js' ?>"></script>
         <script>
+
             localStorage.setItem('base_url', '<?php echo base_url() ?>');
             toggleMainMenu();
 
@@ -32,6 +33,12 @@
                 toggleButton();
             <?php } ?>
 
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-43305108-4', 'auto');
+            ga('send', 'pageview');
 
         </script>
     </body>
