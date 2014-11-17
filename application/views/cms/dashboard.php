0929daa088119593f16d07686438c7160edce731
raul <raul@zamacona.me>
2014-11-17 17:55:13 -0600

    panales y tablas dashboard

---
+++ application/views/cms/dashboard.php
@@ -0,0 +1,96 @@
+<div class="[ contenido ] [ container ]">
+    <div class="[ row ]">
+        <div class="[ col-xs-12 col-md-10 ] [ center-block margin-bottom ]">
+            <h2 class="[ text-center ] [ margin-bottom-small ]">Panel de administración</h2>
+            <h3 class="[ text-center ] [ margin-bottom-big ]"><small>Usuario: <?php echo $_SESSION['username']; ?></small> </h3>
+            <div class="[ row ]">
+                <div class="[ col-xs-4 ] [ margin-bottom ]">
+                    <p class="[ text-center ]">Total de Invitaciones</p>
+                    <p class="[ text-large text-center ]"><?php echo $total_invitations ?></p>
+                </div>
+                <div class="[ col-xs-4 ] [ margin-bottom ]">
+                    <p class="[ text-center ]">Invitaciones aceptadas</p>
+                    <p class="[ text-large text-center ]"><?php echo $total_accepted_invitations ?></p>
+                </div>
+                <div class="[ col-xs-4 ] [ margin-bottom ]">
+                    <p class="[ text-center ]">Invitaciones pendientes</p>
+                    <p class="[ text-large text-center ]"><?php echo $total_pending_invitations ?></p>
+                </div>
+                <div class="[ col-xs-4 ] [ margin-bottom ]">
+                    <p class="[ text-center ]">Invitaciones rechazadas</p>
+                    <p class="[ text-large text-center ]"><?php echo $total_rejected_invitations ?></p>
+                </div>
+                <div class="[ col-xs-4 ] [ margin-bottom ]">
+                    <p class="[ text-center ]">Mensajes enviados</p>
+                    <p class="[ text-large text-center ]"><?php echo $total_sent_messages ?></p>
+                </div>
+                <div class="[ col-xs-4 ] [ margin-bottom ]">
+                    <p class="[ text-center ]">Grupos de intercambio</p>
+                    <p class="[ text-large text-center ]"><?php echo $total_exchange_groups ?></p>
+                </div>
+                <div class="[ col-xs-4 ] [ margin-bottom ]">
+                    <p class="[ text-center ]">Usuarios</p>
+                    <p class="[ text-large text-center ]"><?php echo $total_fb_users ?></p>
+                </div>
+                <div class="[ col-xs-4 ] [ margin-bottom ]">
+                    <p class="[ text-center ]">Intercambio terminados</p>
+                    <p class="[ text-large text-center ]"><?php echo $total_closed_exchange_groups ?></p>
+                </div>
+                <div class="[ col-xs-4 ] [ margin-bottom ]">
+                    <p class="[ text-center ]">Usuarios promedio por grupo</p>
+                    <p class="[ text-large text-center ]"><?php echo $average_users_per_group ?></p>
+                </div>
+                <div class="[ text-center ] [ margin-bottom ]">
+                    <a href="/cms/report_by_group" class="[ btn btn-primary btn-go ] [ text-center ]"><span>Ver información por grupo</span></a>
+                </div><!-- text-center -->
+            </div><!-- row -->
+            <hr class="[ center-block margin-bottom ]">
+            <h3 class="[ text-center margin-bottom ]">Reportes por fecha</h4>
+            <div class="[ text-center ]">
+                <form class="[ white ] [ margin-bottom ] [ j-get-reports ]">
+                    <div class="[ row ]">
+                        <div class="[ col-xs-6 ] [ form-group ] [ margin-bottom ]">
+                            <label class="[ center-block ]" for="start_date">Fecha inicial</label>
+                            <input type="text" class="[ form-control ] [ j-datepicker j-start_date ]" name="start_date">
+                        </div>
+                        <div class="[ col-xs-6 ] [ form-group ] [ margin-bottom ]">
+                            <label class="[ center-block ]" for="end_date">Fecha final</label>
+                            <input type="text" class="[ form-control ] [ j-datepicker j-end_date ]" name="end_date">
+                        </div>
+                    </div><!-- row -->
+                    <button class="[ btn btn-primary btn-go ]"><span>Buscar</span></button>
+                </form>
+            </div>
+            <h4 class="[ text-center ]">Resultados</h5>
+            <div class="[ results ]">
+                <div class="[ row ]">
+                    <div class="[ col-xs-6 ] [ margin-bottom-big ]">
+                        <h4 class="[ text-center ]">Invitaciones aceptadas</h4>
+                        <p class="[ notice ] [ text-center ] [ j-invitaciones-aceptadas ]"></p>
+                        <canvas id="accepted_invitations_per_date"></canvas>
+                    </div>
+                    <div class="[ col-xs-6 ] [ margin-bottom-big ]">
+                        <h4 class="[ text-center ]">Invitaciones pendientes</h4>
+                        <p class="[ notice ] [ text-center ] [ j-invitaciones-pendientes ]"></p>
+                        <canvas id="pending_invitations_per_date"></canvas>
+                    </div>
+                    <div class="[ col-xs-6 ] [ margin-bottom-big ]">
+                        <h4 class="[ text-center ]">Invitaciones rechazadas</h4>
+                        <p class="[ notice ] [ text-center ] [ j-invitaciones-rechazadas ]"></p>
+                        <canvas id="rejected_invitations_per_date"></canvas>
+                    </div>
+                    <div class="[ col-xs-6 ] [ margin-bottom-big ]">
+                        <h4 class="[ text-center ]">Mensajes enviados</h4>
+                        <p class="[ notice ] [ text-center ] [ j-mensajes-enviados ]"></p>
+                        <canvas id="sent_messages_per_date"></canvas>
+                    </div>
+                    <div class="[ col-xs-6 ] [ margin-bottom-big ]">
+                        <h4 class="[ text-center ]">Usuarios</h4>
+                        <p class="[ notice ] [ text-center ] [ j-usuarios ]"></p>
+                        <canvas id="users_per_date"></canvas>
+                    </div>
+                </div><!-- row -->
+            </div><!-- results -->
+        </div>
+    </div>
+</div><!-- cms-dashboard -->
--- application/views/cms/group_reports.php
+++ application/views/cms/group_reports.php
@@ -8,17 +8,30 @@
                 <?php
                 foreach ( $group_info as $key => $info) {
                     if($current_group != $info['exchange_group_name']){
-                        if ( $current_group != '' ){ echo '</table></article>'; }
+                        if ( $current_group != '' ){ echo '</table></div></article>'; }
                         $current_group = $info['exchange_group_name']; ?>
                         <article class="[ margin-bottom-big ]">
-                            <div class="[ info ] [ margin-bottom ]">
-                                <h4>Nombre: "<?php echo $info['exchange_group_name']; ?>"</h4>
-                                <p>Descripción: <?php echo $info['description']; ?> </p>
-                                <p>Número de participantes: <?php echo $info['number_of_members']; ?> </p>
-                                <p>Fecha del intercambio: <?php echo str_replace("00:00:00","",$info['exchange_date']);?> </p>
-                                <p>Último día para unirse: <?php echo str_replace("00:00:00","",$info['join_deadline']); ?> </p>
-                                <p>Presupuesto: <?php if( $info['budget'] == 0 ){ echo 'Sin límite'; } else { echo '$'.$info['budget']; }  ?> </p>
-                            </div><!-- info -->
+                            <div class="panel panel-default">
+                            <div class="panel-heading"><h4 class="[ text-center ]">"<?php echo $info['exchange_group_name']; ?>"</h4></div>
+                            <div class="panel-body">
+                                <p><?php echo $info['description']; ?></p>
+                            </div>
+                            <table class="[ table table-condensed ]">
+                                <thead>
+                                    <tr>
+                                        <th>Participantes</th>
+                                        <th>Fecha del intercambio</th>
+                                        <th>Último día para unirse</th>
+                                        <th>Presupuesto</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <td><?php echo $info['number_of_members']; ?></td>
+                                    <td><?php echo str_replace("00:00:00","",$info['exchange_date']);?></td>
+                                    <td><?php echo str_replace("00:00:00","",$info['join_deadline']); ?></td>
+                                    <td><?php if( $info['budget'] == 0 ){ echo 'Sin límite'; } else { echo '$'.$info['budget']; }  ?></td>
+                                </tbody>
+                            </table>
                             <table class="[ table table-condensed ]">
                                 <thead>
                                     <tr>
@@ -35,8 +48,9 @@
                                 <td><?php echo $info['email']; ?></td>
                                 <td><?php if( $info['admin'] == 1 ){ echo 'Sí'; } else { echo 'No'; } ?>    </td>
                             </tr><!-- fila -->
-                    <?php } ?>
-                    </table><!-- table -->
+                        <?php } ?>
+                        </table><!-- table -->
+                    </div>
                 </article>
             </div><!-- row -->
         </div>
