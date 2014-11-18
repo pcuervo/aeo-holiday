e684345ff9be88e497b06c2ca5441fd977d75ef9
raul <raul@zamacona.me>
2014-11-17 18:49:09 -0600

    catalogo view

--- application/views/cms/catalog.php
+++ application/views/cms/catalog.php
@@ -1,12 +1,12 @@
 <div class="[ contenido ] [ container ]">
     <div class="[ row ] [ login ]">
         <div class="[ col-xs-12 col-md-10 ] [ center-block margin-bottom ]">
-            <h2 class="[ text-center ] [ margin-bottom-big ]">Inicio</h2>
+            <h2 class="[ text-center ] [ margin-bottom-big ]">Catálogo</h2>
             <div class="[ row ]">
                 <div class="[ col-xs-12 ] [ margin-bottom ]">
                     <div class="text-center">
-                        <a class="[ btn btn-primary btn-lg btn-go ] [ margin-bottom ] [ text-center ]" href="/cms/reports"><span>Ver reportes</span></a>
-                        <a class="[ btn btn-primary btn-lg btn-go ] [ margin-bottom ] [ text-center ]" href="/cms/catalog"><span>Gestionar productos catálogo</span></a>
+                        <a class="[ btn btn-primary btn-lg btn-go ] [ margin-bottom ] [ text-center ]" href="/cms/reports"><span>Agregar producto</span></a>
+                        <a class="[ btn btn-primary btn-lg btn-go ] [ margin-bottom ] [ text-center ]" href="/cms/catalog"><span>Ver productos</span></a>
                     </div>
                 </div>
             </div><!-- row -->
